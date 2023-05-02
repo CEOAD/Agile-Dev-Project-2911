b6d9885a676f8e32a3c73dd89c2a37ab9ad62008
Deborah Lao <dlao7@my.bcit.ca>
2023-05-02 00:20:17 -0700

    Adds basic drop down menu for favourite places. Moves the map to the city in current static list based on coordinates.

--- Agile-Dev-Project-2911/Pages/homepage/homepage.js
+++ Agile-Dev-Project-2911/Pages/homepage/homepage.js
@@ -1,5 +1,6 @@
 let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
+let favs = document.querySelector("#chosen");
 
 navToggle.addEventListener("click", function () {
   if (navWrapper.classList.contains("active")) {
@@ -12,3 +13,20 @@ navToggle.addEventListener("click", function () {
     this.setAttribute("aria-expanded", "true");
   }
 });
+
+let cityArray = [{city: "Vancouver", coord: {lat: 49.246292, lng: -123.116226}},
+ {city: "Burnaby", coord: {lat: 49.246445, lng: -122.994560}},
+ {city: "Chilliwack", coord: {lat:49.157940, lng:-121.951469}}]
+
+function areaSelector (event){
+  for (city of cityArray){
+    if (event.target.textContent === city["city"]){
+      map.setCenter(city["coord"]);
+      map.setZoom(12);
+    }  
+  }
+} 
+
+favs.addEventListener("click", areaSelector);
+
+
