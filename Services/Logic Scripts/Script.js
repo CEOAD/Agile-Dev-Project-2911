c24527f48937f0739f81112c70331955744c368e
denzd <denzdvr@gmail.com>
2023-05-04 00:18:30 -0700

    The main weather card has been done. The .upper flap is also done but is static for the time being.

--- Services/Logic Scripts/Script.js
+++ Services/Logic Scripts/Script.js
@@ -1,11 +1,16 @@
 
-function initMap() {
+async function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 0, lng: 0 },
         zoom: 3,
     });
 
-    const cardElement = createCardElement();
+    const mainWeatherData = await fetchWeatherData(0, 0);
+    if (!mainWeatherData) {
+        console.error('Unable to fetch main weather data');
+        return;
+    }
+    const cardElement = createWeatherCard(mainWeatherData);
     marker = createMarker(0, 0, cardElement, true);
 
     marker.addListener("dragend", () => handleMarkerUpdate(cardElement));
@@ -88,7 +93,18 @@ function createMarker(lat, lng, markerElement, draggable = false) {
     overlay.onAdd = function () {
         const panes = overlay.getPanes();
         panes.markerLayer.appendChild(markerElement);
+
+        // Add a click listener to the markerElement
+        markerElement.addEventListener('click', () => {
+            markerElement.style.zIndex = '1000';
+        });
+
+        // Add a click listener to the map to reset the zIndex when clicking outside the markerElement
+        map.addListener('click', () => {
+            markerElement.style.zIndex = '';
+        });
     };
+
     overlay.draw = function () {
         const position = marker.getPosition();
         const projection = overlay.getProjection();
@@ -101,6 +117,10 @@ function createMarker(lat, lng, markerElement, draggable = false) {
         if (oldMainCard) {
             oldMainCard.remove();
         }
+
+        // Remove any previous marker elements in the card container
+        const previousMarkerElements = document.querySelectorAll(".card-container .cardm");
+        previousMarkerElements.forEach((element) => element.remove());
 
         // Update the position of the main card element
         const cardContainer = document.querySelector(".card-container");
@@ -212,56 +232,13 @@ async function handleMarkerUpdate(markerElement) {
         return;
     }
 
-    const cityName = mainWeatherData.name;
-    const country = mainWeatherData.sys.country;
-    const temperature = mainWeatherData.main.temp;
-    const weatherDescription = mainWeatherData.weather[0].description;
-
     // For the main city weather data
-    // Create a new card element with the structure you want
+    // Create a new card element with the structure
     const mainCardElement = createWeatherCard(mainWeatherData);
     markerElement.appendChild(mainCardElement);
 
     clearPreviousCityCards();
-    const locationName = mainWeatherData.name;
-    const windSpeed = mainWeatherData.wind.speed;
-    const mainHumidity = mainWeatherData.main.humidity;
-    const mainAqi = mainWeatherData.visibility;
-    const mainPressure = mainWeatherData.pressure;
-    const mainRealFeel = mainWeatherData.main.feels_like;
-    const mainWeatherIconCode = mainWeatherData.weather[0].icon;
-    markerElement.innerHTML = "";
-    const mainWeatherIcon = document.createElement("img");
-    mainWeatherIcon.src = getWeatherIconURL(mainWeatherIconCode);
-    mainWeatherIcon.className = "weather-icon";
-    markerElement.appendChild(mainWeatherIcon);
 
-    const upper = document.createElement('div')
-    const humidity = document.createElement('div')
-    const pressure = document.createElement('div')
-    const humidityicon = document.createElement('div')
-    const humiditytext = document.createElement('div')
-    const pressureicon = document.createElement('div')
-    const pressuretext = document.createElement('div')
-    pressuretext.textContent = `Pressure`
-    const pressurecontent = document.createElement('div')
-    // pressurecontent.textContent`${}`
-
-    const mainTemperature = mainWeatherData.main.temp;
-    const temperatureElement = document.createElement("div");
-    temperatureElement.className = "temperature";
-    temperatureElement.textContent = `${mainTemperature}°C`;
-    markerElement.appendChild(temperatureElement);
-
-    const locationNameElement = document.createElement("div");
-    locationNameElement.className = "location-name";
-    locationNameElement.textContent = locationName;
-    markerElement.appendChild(locationNameElement);
-
-    const windSpeedElement = document.createElement("div");
-    windSpeedElement.className = "wind-speed";
-    windSpeedElement.textContent = `Wind: ${windSpeed} m/s`;
-    markerElement.appendChild(windSpeedElement);
 
     const nearbyCities = await fetchNearbyCities(coordinates.lat(), coordinates.lng());
 
@@ -294,11 +271,10 @@ function createWeatherCard(weatherData) {
     const cityName = weatherData.name;
     const country = weatherData.sys.country;
     const temperature = weatherData.main.temp;
-    const weatherDescription = weatherData.weather[0].description;
     const windSpeed = weatherData.wind.speed;
     const mainHumidity = weatherData.main.humidity;
     const mainAqi = weatherData.visibility;
-    const mainPressure = weatherData.pressure;
+    const mainPressure = weatherData.main.pressure;
     const mainRealFeel = weatherData.main.feels_like;
     const mainWeatherIconCode = weatherData.weather[0].icon;
 
@@ -320,21 +296,80 @@ function createWeatherCard(weatherData) {
 
     const tempAndDescription = document.createElement("div");
     tempAndDescription.className = "main";
-    tempAndDescription.textContent = `${temperature}°C, ${weatherDescription}`;
+    tempAndDescription.textContent = `${temperature}°C`;
     card.appendChild(tempAndDescription);
 
     const upper = document.createElement('div')
     upper.className = "upper"
 
     const wind = document.createElement("div");
+    const windSvg = `<svg class="wind_svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30" xml:space="preserve">  <image id="image0" width="30" height="30" x="0" y="0" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAMAAAAM7l6QAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+            AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAABaFBMVEUAAAAA//8ilfIhlfMg
+            lvIglfMglvIeku8cjf8glvMhlfIflvMhlfIhlvIglvMhl/MglvIglfIglPEfmfIhlfIglvQfn/8g
+            lfIglvIhlfMglfIglvMhl/AhlfIcm/AAf/8qlOkglPYglvIZmf8zmf8hlfIglfIXi+cilPMhlvMg
+            lfQhlvMglvIhlfIgl/MglvMhlvMhlfMhlvIfl+8hlvMhlfMglvMglvI/f/8hlvMilvMelvAglfIg
+            lvMhlvIglPIglvIhlfIkkfUglfMglfMhlvMhlvMilvMjlfEglvMhlfIhlfMglfIflvEnnOshlvIf
+            lPEflfIek/QglvIglvMhlfIime4jlPAglvMglvEhlvMhl/MglfMglfMhlvIak/Edk/UhlvIglfMg
+            l/IglvIglfIilPIhlvMhlfMhk/Eqqv8glvIglfMcl/UhlfMhlvIhlvMhlfIglfIhlfIgl/QflPQh
+            lvP///+FIn/GAAAAdnRSTlMAAVKu1MmNIQmy91ig/Z5s0fo3KP5dCL27Lvm0NvwSAgwf+woFv7oL
+            Q0RGa9L1Vtndx4sgme3FZgTIFiI/hvZld3sch8Tv7kI683nV6DgN4GBQMsycjw8k6nWqRW3cUxMa
+            5dpnfLU87N8mBrxXG5jnibjLoi8YaHuXCQAAAAFiS0dEd0Zk+dcAAAAJcEhZcwAACxMAAAsTAQCa
+            nBgAAAAHdElNRQfnAhEIBBbLW8PtAAABJ0lEQVQoz62RZ1fCMBSG46atomBR1IJ7g+KotKKgxYl7
+            4Z6493x/v6T0QKMtn/p8ec/Nk5Pc5BLiHCWlZeUVlVU21sWBwgvWuho1bqG2Dh6vla0X4ctGQyP8
+            VrqpuUVPCQGXlQ+26tEGtBfpX0JHp73t6kYPs9Db159nYDAEj8RoNxjCQ+xxvuFIgZHRMcemQMi4
+            iP+I8kRU0TUHG9RJqpWYFVPTcahKsc/jkSAzfgNuVvvjk5gj84W7FhZZvYRl4o0apFawGmT0GsKm
+            an0DmzS31O3cQgo75t27SNLYQyRNc/8AhzkROzrOPjCEE1poMk7Pzi8Sl7gyxp5B/Fq4Aa//A7m9
+            y7V6/2Ac+/hEy8CzUb68ysDbezp/rZL5+Pz6NvWh/TgwzV+1HV523WQ81AAAACV0RVh0ZGF0ZTpj
+            cmVhdGUAMjAyMy0wMi0xN1QwODowNDoyMiswMDowML1dmzYAAAAldEVYdGRhdGU6bW9kaWZ5ADIw
+            MjMtMDItMTdUMDg6MDQ6MjIrMDA6MDDMACOKAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDIzLTAy
+            LTE3VDA4OjA0OjIyKzAwOjAwmxUCVQAAAABJRU5ErkJggg=="></image>
+        </svg>`;
+    const parserWind = new DOMParser();
+    const svgDocWind = parserWind.parseFromString(windSvg, "image/svg+xml");
+    const svgElementWind = svgDocWind.querySelector("svg");
+    const windcont = document.createElement('div')
     wind.className = "wind";
     wind.textContent = `Wind: ${windSpeed} m/s`;
-    upper.appendChild(wind);
+    windcont.className = "windcont"
+    windcont.appendChild(wind)
+    windcont.appendChild(svgElementWind)
+    upper.appendChild(windcont);
 
     const humidity = document.createElement("div");
+    const humiditysvg = `<svg xml:space="preserve" viewBox="0 0 30 30" height="30px" width="30px" y="0px" x="0px"
+                             xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" id="Layer_1"
+                             version="1.1" class="humidity_svg">  <image href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAMAAAAM7l6QAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+          AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAABiVBMVEUAAAAAAP9NerV/f39O
+          e7ZQfLZVf6pRfbfL5fdRfbZIbbZmmcxols5nl85OebSPsteLrdVSfLZxl89ok9FqlM5ahsBdicNa
+          hsFcicRhjcdWgbpahsFfi8ZbhsFijsmErOWLt+9xndZcicJahsFahsFdicN5n81xjcZqlNRpls1q
+          lNBfn99pls9nkcxXgrpZgrtik81OebWNsdeMrtZOebRNerVZg7pwmMhNebRKdLRNerZNebHZ8v9o
+          lM9jj8rV7v3W7v1ch7+Ktu6Lt/CEsep7p+Cz1PO+3fqJte5/q+V+quOUvvLY8f+TvfKpzvapzfaq
+          z/aRvPGdxfSVv/LX8P/W8P+32fnK5vyMuPCmzPXW8P6ny/WWv/KOufGawvO22PjJ5vzB4PrU7v6i
+          yPSz1fiYwfKOufDD4funzPXF4vvE4vuOuvHV7/7U7/7G4/uNufCx1Pew0/ev0veu0feQu/G01viP
+          ufF/q+SCrud+quSItO2kyvWjyfVijslrltFmkcyEqtZgjMf///8NXQssAAAAPHRSTlMAAZgCW+EG
+          y+jMBxRaRXHC2H8bX0ry/vrhyvnw0PDHR0Be/e/4/f4SDDNiEFVb0eI5iMHCho7NwI0YOBdy59Cm
+          AAAAAWJLR0SCi7P/RAAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB+cCEBITAJMBs+kAAAFb
+          SURBVCjPY2CAAUYmZgY8gIWVBY8sGzsHJxc2CW4eXiDJx28jIAjiCgnzgoV5ebiBpIiomK2duISk
+          lL2Ng6O0jIwsmCsnIW5nKyYqwiDv5AwELq5uNjY27h6enh5grpcLiHKSZ1BwBgNvH6C0j68zKlCA
+          SfvZgIA/LukAsHQAVDgwCE06OAQoGxoMlQ4Lj0CVdo6MsomKhrJjfGwi0aSdY+NiYcx4G5sEdGkk
+          kGhjk4RHOjnEJgWPtLNvKprL07CpgktHpEfgk/a3ycAnnWmThRDMxpDOscmFi6Xl5aNLF+QUwqWL
+          bIoxogQBSpJskkpwS5cC4yYFp3RZElA6qQwh7VFeAWZXVFYByWpwxNcAueUeQGlFJWUZCZXauloV
+          CRllVdt6NbB0QyOIq6TIoK4BSrWaWpogSltHVw8srW8A4mqoY6R6QyOgrLEJztxiamZuZsGGOztZ
+          WlnD2QBCYbJl9Cx9XAAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMy0wMi0xNlQxODoxOTowMCswMDow
+          MG/wqfUAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjMtMDItMTZUMTg6MTk6MDArMDA6MDAerRFJAAAA
+          KHRFWHRkYXRlOnRpbWVzdGFtcAAyMDIzLTAyLTE2VDE4OjE5OjAwKzAwOjAwSbgwlgAAAABJRU5ErkJggg==" y="0" x="0" height="30"
+                                                                            width="30" id="image0"></image>
+          </svg>`;
+    const parserHumidity = new DOMParser();
+    const svgDocHumidity = parserHumidity.parseFromString(humiditysvg, "image/svg+xml");
+    const svgElementHumidity = svgDocHumidity.querySelector("svg");
+    const humiditycont = document.createElement('div')
+    humiditycont.className = "humiditycont";
     humidity.className = "humidity";
     humidity.textContent = `Humidity: ${mainHumidity}%`;
-    upper.appendChild(humidity);
+    humiditycont.appendChild(svgElementHumidity)
+    humiditycont.appendChild(humidity)
+    upper.appendChild(humiditycont);
     card2.appendChild(upper)
 
     const lower = document.createElement('div')
