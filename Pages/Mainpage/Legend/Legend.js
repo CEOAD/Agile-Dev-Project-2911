80e8531f35cf3aa36bb887ad90c461fb4297f541
Patrick Q <pquach3@my.bcit.ca>
2023-05-18 22:11:41 -0700

    Added an animation on clicking legend button to transition the popup open and closed.

--- Pages/Mainpage/Legend/Legend.js
+++ Pages/Mainpage/Legend/Legend.js
@@ -1,11 +1,17 @@
 let Legend_btn = document.querySelector(".legend_button")
-let Legend_window = document.querySelector(".legend_wrapper")
+let Legend_window = document.querySelector("#legend_wrapper")
 
 Legend_btn.addEventListener("click", function() {
-    if (Legend_window.classList.contains("legend_hidden")) {
-        Legend_window.classList.remove("legend_hidden");
+    if (Legend_window.classList.contains("hidden_legend")) {
+        Legend_window.classList.remove("hidden_legend")
+    }
+
+    if (Legend_window.classList.contains("open_legend")) {
+        Legend_window.classList.remove("open_legend");
+        Legend_window.classList.add("close_legend");
     }
     else {
-        Legend_window.classList.add("legend_hidden");
+        Legend_window.classList.remove("close_legend");
+        Legend_window.classList.add("open_legend");
     }
 });
